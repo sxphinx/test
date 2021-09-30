139f8cbc6411af2d1461471224382692de4efbb6
sxphinx <42745717+sxphinx@users.noreply.github.com>
2021-09-29 20:23:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 var pane = $('#pane'),
     box = $('#box'),
-    w = pane.width() - box.width(),
-    d = {},
-    x = 3;
+    maxValue = pane.width() - box.width(),
+    keysPressed = {},
+    distancePerIteration = 3;
 
-function newv(v,a,b) {
-    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-    return n < 0 ? 0 : n > w ? w : n;
+function calculateNewValue(oldValue, keyCode1, keyCode2) {
+    var newValue = parseInt(oldValue, 10)
+                   - (keysPressed[keyCode1] ? distancePerIteration : 0)
+                   + (keysPressed[keyCode2] ? distancePerIteration : 0);
+    return newValue < 0 ? 0 : newValue > maxValue ? maxValue : newValue;
 }
 
-$(window).keydown(function(e) { d[e.which] = true; });
-$(window).keyup(function(e) { d[e.which] = false; });
+$(window).keydown(function(event) { keysPressed[event.which] = true; });
+$(window).keyup(function(event) { keysPressed[event.which] = false; });
 
 setInterval(function() {
     box.css({
-        left: function(i,v) { return newv(v, 37, 39); },
-        top: function(i,v) { return newv(v, 38, 40); }
+        left: function(index ,oldValue) {
+            return calculateNewValue(oldValue, 37, 39);
+        },
+        top: function(index, oldValue) {
+            return calculateNewValue(oldValue, 38, 40);
+        }
     });
-}, /*20*/);
+}, 20);
